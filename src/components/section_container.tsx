38dccb354ad112d0a71e55287a43066c5ff4b991
Stephen Huang <stephen@Stephens-MacBook-Air.local>
2022-09-10 01:09:17 -0400

    Moved files

--- src/components/section_container.tsx
+++
@@ -1,9 +0,0 @@
-interface SectionContainerProperties {
-  children: React.ReactNode;
-}
-
-const SectionContainer = (props: SectionContainerProperties) => {
-  return <div className="section">{props.children}</div>;
-};
-
-export default SectionContainer;
